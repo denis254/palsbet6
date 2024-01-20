c31b9c72b61f704ea1882a198e67f22667bca551
Denis <deniskiprono4@gmail.com>
2024-01-20 21:24:00 +0300

    my changes

--- palsbet/settings.py
+++ palsbet/settings.py
@@ -164,8 +164,8 @@ ADMINS = (('Keneth', 'palsbet1@gmail.com'),)
 
 CSRF_TRUSTED_ORIGINS = ['http://palsbet.com/', 'https://palsbet.com/', 'http://palsbet.pythonanywhere.com/', 'http://127.0.0.1']
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+#SECURE_SSL_REDIRECT = True
 
 
 # adding config
